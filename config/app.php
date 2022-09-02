bc24b05a65aa7f6eaa4101b6c7bb4dd064d903b9
bluekyeet <56775066+bluekyeet@users.noreply.github.com>
2022-09-02 19:54:11 +1200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -9,7 +9,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => '3.4.2',
+    'version' => '3.1',
 
     /*
     |--------------------------------------------------------------------------
@@ -21,7 +21,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Pterodactyl'),
+    'name' => env('APP_NAME', 'BluedHost'),
 
     /*
     |--------------------------------------------------------------------------
